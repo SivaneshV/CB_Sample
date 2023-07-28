2d795daffd1e2fda56f88474d7229c8b03819e14
SivaneshV <87308952+SivaneshV@users.noreply.github.com>
2023-07-28 14:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./CB_Sample')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route("/")
 def hello():
     return "Hello there. I am Clever Brain Updated 3."
